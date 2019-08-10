3bbb9ce5c77662114f13c07ff110453dfbe0a6ec
jay depala <jaidepala@github.com>
2019-08-11 03:49:06 +0530

    added search bar

---
+++ client/src/modules/dashboard/dashboard.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+// import axios from "axios";
+
+class Dashboard extends Component {
+
+    constructor(props) {
+
+        super(props);
+    };
+
+    render() {
+
+        return (
+            <div>
+                Dashboard Works!
+            </div>
+        );
+    };
+}
+
+export default Dashboard;
---
+++ client/src/modules/login/login.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+// import axios from "axios";
+
+class Login extends Component {
+
+    constructor(props) {
+
+        super(props);
+    };
+
+    render() {
+
+        return (
+            <div>
+                Login Works!
+            </div>
+        );
+    };
+}
+
+export default Login;
