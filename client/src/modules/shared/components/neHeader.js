e583662da6041d169053ad35423094d48e8163c2
jay depala <jaidepala@github.com>
2019-11-04 23:50:20 +0530

    styling on header

--- client/src/modules/shared/components/neHeader.js
+++ client/src/modules/shared/components/neHeader.js
@@ -328,66 +328,69 @@ class NeHeader extends Component {
                                 <Toolbar>
                                     <IconButton
                                         edge="start"
+                                        className={classes.menuButton}
                                         onClick={this.toggleDrawer(true)}
                                         color="inherit"
                                         aria-label="open drawer">
 
                                         <MenuIcon />
                                     </IconButton>
-                                    <Typography variant="h6" noWrap>
+                                    <Typography className={ loggedIn ? classes.title : ''} variant="h6" noWrap>
                                         Weight Tracker
                                     </Typography>
                                     {
                                         loggedIn && (
-                                            <div className={classes.search}>
-                                                <div className={classes.searchIcon}>
-                                                    <SearchIcon />
+                                            <div className="menu-action-container">
+                                                <div className={classes.search}>
+                                                    <div className={classes.searchIcon}>
+                                                        <SearchIcon />
+                                                    </div>
+                                                    <InputBase
+                                                        placeholder="Search…"
+                                                        classes={{
+                                                            root: classes.inputRoot,
+                                                            input: classes.inputInput,
+                                                        }}
+                                                        inputProps={{ 'aria-label': 'search' }}
+                                                    />
                                                 </div>
-                                                <InputBase
-                                                    placeholder="Search…"
-                                                    classes={{
-                                                        root: classes.inputRoot,
-                                                        input: classes.inputInput,
-                                                    }}
-                                                    inputProps={{ 'aria-label': 'search' }}
-                                                />
+                                                <div className={classes.grow} />
+                                                <div className={classes.sectionDesktop}>
+                                                    <IconButton aria-label="show 4 new mails" color="inherit">
+                                                        <Badge badgeContent={4} color="secondary">
+                                                            <MailIcon />
+                                                        </Badge>
+                                                    </IconButton>
+                                                    <IconButton aria-label="show 17 new notifications" color="inherit">
+                                                        <Badge badgeContent={17} color="secondary">
+                                                            <NotificationsIcon />
+                                                        </Badge>
+                                                    </IconButton>
+                                                    <IconButton
+                                                        edge="end"
+                                                        aria-label="account of current user"
+                                                        aria-controls={menuId}
+                                                        aria-haspopup="true"
+                                                        onClick={this.handleProfileMenuOpen}
+                                                        color="inherit">
+
+                                                        <AccountCircle />
+                                                    </IconButton>
+                                                </div>
+                                                <div className={classes.sectionMobile}>
+                                                    <IconButton
+                                                        aria-label="show more"
+                                                        aria-controls={mobileMenuId}
+                                                        aria-haspopup="true"
+                                                        onClick={this.handleMobileMenuOpen}
+                                                        color="inherit">
+
+                                                        <MoreIcon />
+                                                    </IconButton>
+                                                </div>
                                             </div>
                                         )
                                     }
-                                    <div className={classes.grow} />
-                                    <div className={classes.sectionDesktop}>
-                                        <IconButton aria-label="show 4 new mails" color="inherit">
-                                            <Badge badgeContent={4} color="secondary">
-                                                <MailIcon />
-                                            </Badge>
-                                        </IconButton>
-                                        <IconButton aria-label="show 17 new notifications" color="inherit">
-                                            <Badge badgeContent={17} color="secondary">
-                                                <NotificationsIcon />
-                                            </Badge>
-                                        </IconButton>
-                                        <IconButton
-                                            edge="end"
-                                            aria-label="account of current user"
-                                            aria-controls={menuId}
-                                            aria-haspopup="true"
-                                            onClick={this.handleProfileMenuOpen}
-                                            color="inherit">
-
-                                            <AccountCircle />
-                                        </IconButton>
-                                    </div>
-                                    <div className={classes.sectionMobile}>
-                                        <IconButton
-                                            aria-label="show more"
-                                            aria-controls={mobileMenuId}
-                                            aria-haspopup="true"
-                                            onClick={this.handleMobileMenuOpen}
-                                            color="inherit">
-
-                                            <MoreIcon />
-                                        </IconButton>
-                                    </div>
                                 </Toolbar>
                             </AppBar>
                             { this.renderMobileMenu() }
